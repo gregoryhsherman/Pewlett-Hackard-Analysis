7c468592cfcd8bd2e62b437b2a4d768058816193
Greg Sherman <6807242+gregoryhsherman@users.noreply.github.com>
2020-08-26 13:37:16 -0400

    message

--- schema.sql
+++ schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE Employees (
 );
 
 CREATE TABLE dept_manager (
-	dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -35,25 +35,38 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+DROP TABLE titles
+
 CREATE TABLE dept_emp (
-	dept_no VARCHAR NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,	
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
-	emp_no INT NOT NULL,
-	title INT NOT NULL,
+	emp_no VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
 	PRIMARY KEY (emp_no, title, from_date)
 );
 
-SELECT * FROM departments;
+
+SELECT * FROM salaries;
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
